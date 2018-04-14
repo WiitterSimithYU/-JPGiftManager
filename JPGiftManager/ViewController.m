f5d917edd1cc3968426a8a0f8956d3f11642356b
WiitterSimithYU <378961614@qq.com>
2018-04-14 14:43:08 +0800

    commit

---
+++ JPGiftManager/ViewController.m
@@ -0,0 +1,119 @@
+//
+//  ViewController.m
+//  JPGiftManager
+//
+//  Created by Keep丶Dream on 2018/3/13.
+//  Copyright © 2018年 dong. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "JPGiftView.h"
+#import "JPGiftCellModel.h"
+#import "YYModel.h"
+#import "JPGiftModel.h"
+#import "JPGiftShowManager.h"
+#import "UIImageView+WebCache.h"
+
+@interface ViewController ()<JPGiftViewDelegate>
+/** gift */
+@property(nonatomic,strong) JPGiftView *giftView;
+/** gifimage */
+@property(nonatomic,strong) UIImageView *gifImageView;
+@property (weak, nonatomic) IBOutlet UIButton *changeBtn;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *filePath=[[NSBundle mainBundle]pathForResource:@"data" ofType:@"json"];
+    NSData *jsonData = [NSData dataWithContentsOfFile:filePath];
+    NSDictionary *responseObject = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:nil];
+    NSArray *data = [responseObject objectForKey:@"data"];
+    NSMutableArray *dataArr = [NSMutableArray arrayWithArray:data];
+    self.giftView.dataArray = [NSArray yy_modelArrayWithClass:[JPGiftCellModel class] json:dataArr];
+    
+}
+
+- (UIImageView *)gifImageView{
+    
+    if (!_gifImageView) {
+        
+        _gifImageView = [[UIImageView alloc] initWithFrame:CGRectMake(7.5, 0, 360, 225)];
+        _gifImageView.hidden = YES;
+    }
+    return _gifImageView;
+}
+
+- (JPGiftView *)giftView{
+    
+    if (!_giftView) {
+        
+        _giftView = [[JPGiftView alloc] init];
+        _giftView.delegate = self;
+    }
+    return _giftView;
+}
+
+- (IBAction)clickGift:(id)sender {
+    
+    [self.giftView showGiftView];
+}
+- (IBAction)changeGifType:(id)sender {
+    
+    self.changeBtn.selected = !self.changeBtn.selected;
+}
+
+- (void)giftViewSendGiftInView:(JPGiftView *)giftView data:(JPGiftCellModel *)model {
+    
+    NSLog(@"点击-- %@",model.name);
+    JPGiftModel *giftModel = [[JPGiftModel alloc] init];
+    giftModel.userIcon = model.icon;
+    giftModel.userName = model.username;
+    giftModel.giftName = model.name;
+    giftModel.giftImage = model.icon;
+    giftModel.giftGifImage = model.icon_gif;
+    giftModel.giftId = model.id;
+    giftModel.defaultCount = 0;
+    giftModel.sendCount = 1;
+    
+    if (self.changeBtn.selected) {
+        
+        [[JPGiftShowManager sharedManager] showGiftViewWithBackView:self.view info:giftModel completeBlock:^(BOOL finished) {
+            //结束
+        } completeShowGifImageBlock:^(JPGiftModel *giftModel) {
+            //展示gifimage
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                UIWindow *window = [UIApplication sharedApplication].keyWindow;
+                [window addSubview:self.gifImageView];
+                [self.gifImageView sd_setImageWithURL:[NSURL URLWithString:giftModel.giftGifImage]];
+                self.gifImageView.hidden = NO;
+                
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    self.gifImageView.hidden = YES;
+                    [self.gifImageView sd_setImageWithURL:[NSURL URLWithString:@""]];
+                    [self.gifImageView removeFromSuperview];
+                });
+            });
+        }];
+    }else {
+        
+        [[JPGiftShowManager sharedManager] showGiftViewWithBackView:self.view info:giftModel completeBlock:^(BOOL finished) {
+            //结束
+            }
+        ];
+
+    }
+}
+
+
+- (void)giftViewGetMoneyInView:(JPGiftView *)giftView {
+    
+    NSLog(@"充值");
+}
+
+@end
